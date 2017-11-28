cbc23d359048780da6774296731658666a954477
rmag4233 <ryan.agnew@libertymutual.com>
2017-11-28 14:17:11 -0500

    Serializes loans on user and gear resources
    Adds loans to the serializer for gears and users

--- app/serializers/gear_serializer.rb
+++ app/serializers/gear_serializer.rb
@@ -1,4 +1,4 @@
 class GearSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :price, :availability, :image_URL, :user_id
+  attributes :id, :title, :description, :price, :availability, :image_URL, :user_id, :loans
   belongs_to :user
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,5 @@
 
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :gears
+  has_many :loans
 end
