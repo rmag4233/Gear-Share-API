3e3182d9a7460f005046add32a2d7cd4b2389dca
rmag4233 <ryan.agnew@libertymutual.com>
2017-11-28 17:30:15 -0500

    Defines routes file for loans resource
    Defines the routes file for loans resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :loans
+  resources :loans, except: %i[new edit]
   resources :gears, except: %i[new edit]
   resources :examples, except: %i[new edit]
   post '/sign-up' => 'users#signup'
